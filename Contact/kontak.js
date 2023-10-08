7f3df87c20f68ffd0b484f31892e29bc1f5afffe
TasyaNurulLaily <tasyalaily8@gmail.com>
2023-10-08 09:40:05 -0700

    Update Contact

--- Contact/kontak.js
+++ Contact/kontak.js
@@ -10,38 +10,37 @@
 });
 
 $(document).ready(function() {
-    // Menggunakan jQuery untuk menangani pengiriman form
-    $("form").submit(function(event) {
-      event.preventDefault(); // Mencegah pengiriman form default
-  
-      // Dapatkan data dari form
-      var firstName = $("#firstName").val();
-      var lastName = $("#lastName").val();
-      var phoneNumber = $("#phoneNumber").val();
-      var email = $("#email").val();
-      var purpose = $("input[name='purpose']:checked").map(function() {
-        return $(this).val();
-      }).get().join(', ');
-      var message = $("#message").val();
-  
-      // Buat pesan notifikasi berdasarkan data yang diisi oleh pengguna
-      var notificationMessage = "Terima kasih, " + firstName + " " + lastName + ". ";
-      notificationMessage += "Kami akan menghubungi Anda melalui email (" + email + ") atau telepon (" + phoneNumber + ") ";
-      notificationMessage += "mengenai tujuan Anda: " + purpose + ". Pesan Anda: " + message;
-  
-      // Tampilkan pesan notifikasi
-      alert(notificationMessage); // Menggunakan alert untuk menampilkan notifikasi
-  
-      // Mengosongkan nilai-nilai input setelah pengiriman berhasil
-      $("#firstName").val("");
-      $("#lastName").val("");
-      $("#phoneNumber").val("");
-      $("#email").val("");
-      $("input[name='purpose']").prop('checked', false);
-      $("#message").val("");
-    });
+  // Menggunakan jQuery untuk menangani pengiriman form
+  $("form").submit(function(event) {
+    event.preventDefault(); // Mencegah pengiriman form default
+
+    // Dapatkan data dari form
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var phoneNumber = $("#phoneNumber").val();
+    var email = $("#email").val();
+    var purpose = $("input[name='purpose']:checked").map(function() {
+      return $(this).val();
+    }).get().join(', ');
+    var message = $("#message").val();
+
+    // Buat pesan notifikasi berdasarkan data yang diisi oleh pengguna
+    var notificationMessage = "Terima kasih, " + firstName + " " + lastName + ". ";
+    notificationMessage += "Kami akan menghubungi Anda melalui email (" + email + ") atau telepon (" + phoneNumber + ") ";
+    notificationMessage += "mengenai tujuan Anda: " + purpose + ". Pesan Anda: " + message;
+
+    // Tampilkan pesan notifikasi
+    alert(notificationMessage); // Menggunakan alert untuk menampilkan notifikasi
+
+    // Mengosongkan nilai-nilai input setelah pengiriman berhasil
+    $("#firstName").val("");
+    $("#lastName").val("");
+    $("#phoneNumber").val("");
+    $("#email").val("");
+    $("input[name='purpose']").prop('checked', false);
+    $("#message").val("");
   });
-  
+});
 
 $(document).ready(function() {
 $("form.search-form").submit(function(event) {
